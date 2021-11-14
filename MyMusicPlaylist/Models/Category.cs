1fb972edbe9a00ecc1eaa8466b2b5aecfe599368
Ema <Ema@DESKTOP-SV4UP93>
2021-11-14 14:35:33 +0100

    Add project files.

---
+++ MyMusicPlaylist/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyMusicPlaylist.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ MyMusicPlaylist/Models/Song.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyMusicPlaylist.Models
+{
+    public class Song
+    {
+        [Key]
+        public int SongId { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Performer{ get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Url { get; set; }
+        public int Review { get; set; }
+
+        [Column(TypeName = "bit")]
+        public bool Favourite { get; set; }
+        public DateOnly DateOfInput { get; set; }
+        public DateOnly DateOfLastEdit { get; set; }
+        public Category SongCategory { get; set; }
+    }
+}
